f78593c9386a37819e6175fe03530b76d2a948b3
Maxim Petischak <petrischak.maxim@gmail.com>
2020-08-09 22:02:42 +0300

    Создал моковые данные для точки маршрута

--- src/mock/route.js
+++ src/mock/route.js
@@ -1,67 +1,36 @@
-export const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
+import {CITY_LIST, WAYPOINT_LIST, activity, transfer, OFFER_LIST, TRIP_IMAGE_URL, TRIP_SENTENCE} from "../const.js";
+import {getRandomInteger, getRandomIndex} from "../utils.js";
 
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
+const generateSentence = (maxLength = 5) => {
+  const sentenceQuantity = getRandomInteger(1, maxLength);
+  return TRIP_SENTENCE.repeat(sentenceQuantity);
 };
 
-const generateWayPoint = () => {
-  const wayponintList = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`, `Restaurant`];
-  const randomIndex = getRandomInteger(0, wayponintList.length - 1);
-  return wayponintList[randomIndex];
-};
-
-const getRandomDestination = () => {
-  const cityList = [
-    `Westminster`,
-    `Florida`,
-    `Boise`,
-    `Boston`,
-    `Fort Smith`,
-    `Wilmington`,
-    `Winston`,
-    `Winter Haven`,
-  ];
-  const randomIndex = getRandomInteger(0, cityList.length - 1);
-  return cityList[randomIndex];
-};
-
-const generateSentence = (maxLenght = 5) => {
-  const descriptionSentence = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`;
-  const sentenceQuantity = getRandomInteger(1, maxLenght);
-  return descriptionSentence.repeat(sentenceQuantity);
-};
-
-const generateImage = (maxLenght = 5) => {
-  const imageUrl = `http://picsum.photos/248/152?r=`;
-  const imagesQuantity = getRandomInteger(1, maxLenght);
+const generateImage = (maxLength = 5) => {
+  const imagesQuantity = getRandomInteger(1, maxLength);
   const imagesList = new Array(imagesQuantity).fill().map(()=> {
     const imageParam = getRandomInteger(1, 10);
-    return `${imageUrl}${imageParam}`;
+    return `${TRIP_IMAGE_URL}${imageParam}`;
   });
 
   return [...new Set(imagesList)];
 };
 
-const getRandomOffer = () => {
-  const offersList = [`Add luggage`, `Switch to comfort class`, `Add meal`, `Choose seats`];
-  const randomIndex = getRandomInteger(0, offersList.length - 1);
-  return offersList[randomIndex];
-};
-
 export const generateRoute = () => {
-
   return {
-    waypoint: generateWayPoint(),
-    destination: getRandomDestination(),
+    waypoint: getRandomIndex(WAYPOINT_LIST),
+    waypointTypes: {
+      activity,
+      transfer
+    },
+    destination: getRandomIndex(CITY_LIST),
     cost: getRandomInteger(30, 200),
-    durationOfTrip: null,
     destinationInfo: {
       description: generateSentence(),
       photo: generateImage(),
     },
     offers: {
-      offerDescription: getRandomOffer(),
+      offerDescription: getRandomIndex(OFFER_LIST),
       offerPrice: getRandomInteger(5, 100)
     },
   };
