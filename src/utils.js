be054b0ca4e62ed0bdc54a5c2353d4d59f70998a
Maxim Petrischak <petrischak.maxim@gmail.com>
2020-08-11 19:00:19 +0300

    Добавил функции для форматирования даты

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,7 @@ export const getRandomInteger = (a = 0, b = 1) => {
 };
 
 export const getRandomIndex = (array) => array[getRandomInteger(0, array.length - 1)];
+
+export const getDateTimeFormat = (date) => date.toJSON().substring(0, 10).replace(`T`, ` `);
+
+export const getFormatedDate = (date) => date.toLocaleString(`en-US`, {month: `short`, day: `numeric`});
