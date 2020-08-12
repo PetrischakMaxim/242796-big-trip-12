ba08868ec5dd253a82c6606ea1219fad5cf4ba5f
Maxim Petrischak <petrischak.maxim@gmail.com>
2020-08-12 19:01:41 +0300

    Начал работу с датами

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,10 @@ export const getRandomInteger = (a = 0, b = 1) => {
 
 export const getRandomIndex = (array) => array[getRandomInteger(0, array.length - 1)];
 
-export const getDateTimeFormat = (date) => date.toJSON().substring(0, 10).replace(`T`, ` `);
+export const getDateTimeFormat = (date) => date.toJSON().split(`T`)[0];
+
+export const getDateAndTimeFormat = (date) => date.toJSON().split(`.`)[0];
+
+export const getTimeFormat = (date) => `${date.getHours()}:${date.getMinutes()}`;
 
 export const getFormatedDate = (date) => date.toLocaleString(`en-US`, {month: `short`, day: `numeric`});
