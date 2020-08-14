b342fe5099af4a66a8c7638cd3e181c590097983
Maxim Petrischak <petrischak.maxim@gmail.com>
2020-08-14 10:09:25 +0300

    Рефакторинг функций для улучшения читабельности

--- src/components/event/event-day.js
+++ src/components/event/event-day.js
@@ -2,13 +2,18 @@ import {getDateTimeFormat, getFormatedDate} from "../../utils.js";
 import {createEventList} from "./event-list.js";
 
 export const createEventDay = (date, count) => {
-  return `<li class="trip-days__item day">
-  <div class="day__info">
-    <span class="day__counter">${count}</span>
-    <time class="day__date" datetime="${getDateTimeFormat(date)}">${getFormatedDate(date).toLowerCase()}</time>
-  </div>
-  ${createEventList()}
-</li>
-
-`;
+  const dateTime = getDateTimeFormat(date);
+  const formatedDate = getFormatedDate(date).toLowerCase();
+  return `
+  <li class="trip-days__item day">
+    <div class="day__info">
+      <span class="day__counter">
+        ${count}
+      </span>
+      <time class="day__date" datetime="${dateTime}">
+        ${formatedDate}
+      </time>
+    </div>
+    ${createEventList()}
+  </li>`;
 };
