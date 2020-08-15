94fff5a3f6caa30c1c94bf2fbe1d6038a9fb2c50
Maxim Petischak <petrischak.maxim@gmail.com>
2020-08-15 16:29:21 +0300

    Реализовал переключение точки маршрута на форму редактирования

--- src/components/event/event-form.js
+++ src/components/event/event-form.js
@@ -43,7 +43,8 @@ const createEventFormTemplate = (route) => {
     </div>`;
   };
 
-  return `<form class="trip-events__item  event  event--edit" action="#" method="post">
+  return `<li class="trip-events__item">
+  <form class="event  event--edit" action="#" method="post">
     <header class="event__header">
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
@@ -70,7 +71,8 @@ const createEventFormTemplate = (route) => {
       <button class="event__reset-btn" type="reset">Cancel</button>
     </header>
     ${createTripEventsTemplate()}
-  </form>`;
+  </form>
+  </li>`;
 };
 
 
--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import {generateRoute} from "./mock/route.js";
 import {render, RenderPosition} from "./utils.js";
 
 const {AFTERBEGIN, BEFOREEND} = RenderPosition;
-const TASK_COUNT = 20;
+const TASK_COUNT = 8;
 const routes = new Array(TASK_COUNT).fill().map(generateRoute);
 
 const pageHeaderElement = document.querySelector(`.page-header`);
@@ -27,15 +27,38 @@ const pageMainElement = document.querySelector(`.page-main`);
 const pageMainContainer = pageMainElement.querySelector(
     `.page-body__container`
 );
-const tripEventsElement = pageMainContainer.querySelector(`.trip-events`);
 
+const tripEventsElement = pageMainContainer.querySelector(`.trip-events`);
 render(tripEventsElement, new SortFormView().getElement(), BEFOREEND);
+
+const renderEvent = (eventListElement, route) => {
+  const eventComponent = new EventItemView(route);
+  const eventFormComponent = new EventFormView(route);
+
+  const replaceEventItemState = (newElement, oldElement) => {
+    eventListElement.replaceChild(newElement.getElement(), oldElement.getElement());
+  };
+
+  const eventComponentBtn = eventComponent.getElement().querySelector(`.event__rollup-btn`);
+  eventComponentBtn.addEventListener(`click`, () => {
+    replaceEventItemState(eventFormComponent, eventComponent);
+  });
+
+  const formComponentBtn = eventFormComponent.getElement().querySelector(`.event__reset-btn`);
+  formComponentBtn.addEventListener(`click`, () => {
+    replaceEventItemState(eventComponent, eventFormComponent);
+  });
+
+  render(eventListElement, eventComponent.getElement(), BEFOREEND);
+};
+
+
 const dayListComponent = new EventDayListView();
 render(pageMainContainer, dayListComponent.getElement(), BEFOREEND);
 
 let dayCounter = 1;
 let routeIndex = 0;
-const {start: startDate} = routes[1].tripDates;
+const {start: startDate} = routes[0].tripDates;
 let currentDay = startDate.getDate();
 const lastDay = routes[routes.length - 1].tripDates.start.getDate();
 let currentDate = startDate;
@@ -50,7 +73,7 @@ for (let day = currentDay; day <= lastDay; day++) {
   for (let i = routeIndex; i < TASK_COUNT; i++) {
     const {start} = routes[i].tripDates;
     if (start.getDate() === currentDay) {
-      render(tripEventListElement, new EventItemView(routes[i]).getElement(), BEFOREEND);
+      renderEvent(tripEventListElement, routes[i]);
     } else {
       currentDay = start.getDate();
       currentDate = start;
