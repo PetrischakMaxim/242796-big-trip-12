525a35d3ed3736c6b8ed27b740536d4ee755fab3
Maxim Petrischak <petrischak.maxim@gmail.com>
2020-08-19 18:06:51 +0300

    Небольшой рефакторинг

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -8,6 +8,7 @@ import NoWaypointView from "../components/event/no-event-waypoint.js";
 import {render, replace} from "../utils/dom-utils.js";
 
 export default class Trip {
+
   constructor(container) {
     this._container = container;
     this._containerInner = this._container.querySelector(`.trip-events`);
@@ -24,9 +25,7 @@ export default class Trip {
     this._routesLength = this._routes.length;
     this._tripCount = count;
 
-    this._renderSort();
-    this._renderDayList();
-    this._renderTrip();
+    this._renderTripBoard();
   }
 
   _renderSort() {
@@ -72,17 +71,15 @@ export default class Trip {
   }
 
   _renderWaypoints() {
-    let dayCounter = 1;
     let routeIndex = 0;
     const {start: startDate} = this._routes[0].tripDates;
     let currentDay = startDate.getDate();
     const lastDay = this._routes[this._routesLength - 1].tripDates.start.getDate();
     let currentDate = startDate;
+    const days = lastDay - currentDay + 1;
 
-    for (let day = currentDay; day <= lastDay; day++) {
-
-      render(this._dayListComponent, new EventDayView(currentDate, dayCounter));
-
+    Array(days).fill().forEach((_, index) => {
+      render(this._dayListComponent, new EventDayView(currentDate, index + 1));
       this._tripEventList = this._getTripDayList(this._dayListComponent);
 
       for (let i = routeIndex; i < this._tripCount; i++) {
@@ -92,12 +89,12 @@ export default class Trip {
         } else {
           currentDay = start.getDate();
           currentDate = start;
-          dayCounter++;
           routeIndex = i;
           break;
         }
       }
-    }
+
+    });
   }
 
   _renderTrip() {
@@ -106,5 +103,11 @@ export default class Trip {
     } else {
       this._renderWaypoints();
     }
+  }
+
+  _renderTripBoard() {
+    this._renderSort();
+    this._renderDayList();
+    this._renderTrip();
   }
 }
