d24e9af6aeb7f6b559cb69cee3f9708bec0322d8
Maxim Petischak <petrischak.maxim@gmail.com>
2020-08-22 16:19:14 +0300

    Добавил функцию генерации уникального id для точек маршрута

--- src/mock/route.js
+++ src/mock/route.js
@@ -1,8 +1,22 @@
-import {CITY_LIST, WAYPOINT_LIST, OFFER_LIST, TRIP_IMAGE_URL, TRIP_SENTENCE, activity, transfer} from "../const.js";
-import {getRandomInteger, getRandomIndex} from "../utils/utils.js";
+import {
+  CITY_LIST,
+  WAYPOINT_LIST,
+  OFFER_LIST,
+  TRIP_IMAGE_URL,
+  TRIP_SENTENCE,
+  activity,
+  transfer
+} from "../const.js";
+
+import {getRandomInteger, getRandomIndex, generateId} from "../utils/utils.js";
 
 const generateOffers = (offers, count) => {
-  return new Array(count).fill().map(() => ({name: getRandomIndex(offers), cost: getRandomInteger(10, 100)}));
+  return new Array(count)
+  .fill()
+  .map(() => ({
+    name: getRandomIndex(offers),
+    cost: getRandomInteger(10, 100)
+  }));
 };
 
 const generateSentence = (maxLength = 5) => {
@@ -12,15 +26,20 @@ const generateSentence = (maxLength = 5) => {
 
 const generateImage = (maxLength = 5) => {
   const imagesQuantity = getRandomInteger(1, maxLength);
-  const imagesList = new Array(imagesQuantity).fill().map(()=> {
-    const imageParam = getRandomInteger(1, 10);
-    return `${TRIP_IMAGE_URL}${imageParam}`;
-  });
+  const imagesList = new Array(imagesQuantity)
+    .fill()
+    .map(()=> {
+      const imageParam = getRandomInteger(1, 10);
+      return `${TRIP_IMAGE_URL}${imageParam}`;
+    });
 
   return [...new Set(imagesList)];
 };
 
-const generateDescription = () => ({description: generateSentence(), images: generateImage()});
+const generateDescription = () => ({
+  description: generateSentence(),
+  images: generateImage()
+});
 
 const maxDaysGap = 5;
 const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
@@ -39,8 +58,11 @@ const generateTripDates = () => {
 };
 
 export const generateRoute = () => {
+
   const wayPoint = getRandomIndex(WAYPOINT_LIST);
+
   return {
+    id: generateId(),
     waypoint: wayPoint,
     hasWaypoint: (wayPoint) ? true : false,
     waypointTypes: {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+export const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
+
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -22,7 +24,6 @@ export const getTimeOfTrip = (d1, d2) => {
   diff /= (60 * 60);
   return Math.abs(Math.round(diff)) + `H`;
 };
-
 
 export const sortPrice = (min, max) => (max.cost - min.cost);
 
