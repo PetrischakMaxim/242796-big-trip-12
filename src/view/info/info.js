ad2be1e42ffa35cfb8dab1e8381720af2f5e2e45
Maxim Petischak <petrischak.maxim@gmail.com>
2020-09-26 02:02:44 +0300

    Добавил презентер для информации о поездках

--- src/view/info/info.js
+++ src/view/info/info.js
@@ -1,50 +1,33 @@
 import AbstractView from "../abstract/abstract.js";
-import {getFormatedDate} from '../../utils/date-utils.js';
 
-const createPointInfoTemplate = (points) => {
-
-  if (!points.length) {
-    return ``;
-  }
-  const separator = (points.length > 3) ? `—...—` : `${points[1].info.name}`;
-  const startPoint = points[0].info.name;
-  const finalPoint = points[points.length - 1].info.name;
-  const startDate = getFormatedDate(points[0].start);
-  const endDate = getFormatedDate(points[points.length - 1].end);
-
-  return (
-    `<div class="trip-info__main">
-      <h1 class="trip-info__title">
-        ${startPoint}${separator}${finalPoint}
-      </h1>
-      <p class="trip-info__dates">${startDate}&nbsp;—&nbsp;${endDate}</p>
-    </div>`
-  );
-};
-
-const createTotalCostTemplate = (points) => (
-  `<p class="trip-info__cost">
-    Total: €&nbsp;
-    <span class="trip-info__cost-value">
-      ${points.map((point)=> point.price).reduce((total, current) => total + current, 0)}
-    </span>
-  </p>`
-);
-
-const createTripInfoTempalte = (points) => (
+const createInfoTemplate = (title, time, cost) => (
   `<section class="trip-main__trip-info trip-info">
-    ${createPointInfoTemplate(points)}
-    ${createTotalCostTemplate(points)}
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">
+          ${(title) ? title : ``}
+        </h1>
+        <p class="trip-info__dates">
+          ${(time) ? time : ``}
+        </p>
+      </div>
+      <p class="trip-info__cost">
+        Total: &euro;&nbsp;
+        <span class="trip-info__cost-value">
+          ${(cost) ? cost : 0}
+        </span>
+      </p>
   </section>`
 );
 
-export default class TripInfo extends AbstractView {
-  constructor(points) {
+export default class Info extends AbstractView {
+  constructor(title, time, cost) {
     super();
-    this._points = points;
+    this._title = title;
+    this._time = time;
+    this._cost = cost;
   }
 
   getTemplate() {
-    return createTripInfoTempalte(this._points);
+    return createInfoTemplate(this._title, this._time, this._cost);
   }
 }
